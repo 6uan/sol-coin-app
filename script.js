9e5d6fe237d5de8c5688f81ef4c1f90ead61c49a
6uan <6uannn@gmail.com>
2024-02-06 13:33:18 -0500

    mobile layout update

--- script.js
+++ script.js
@@ -2,16 +2,35 @@
 
 const TOKEN_URL = 'https://api.dexscreener.com/latest/dex/tokens/JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN';
 const tokenomicsContainter = document.getElementById("tokenomicsContainer");
+const dataContainer = document.getElementById("dataContainer")
 
+async function getToken(){
+    const response = await fetch(TOKEN_URL);
+    const data = await response.json();
+    console.log(data);
+}
 
+getToken();
+
+/*
 fetch(TOKEN_URL)
     .then(response => response.json())
     .then(responseJson => {
         // Assuming you want to iterate over the "pairs" array in the response
 
         let div = document.createElement('div');
+        let liData = document.createElement('div'); 
 
         for (let pair of responseJson.pairs) {
+
+
+            liData.innerHTML = `
+            <li> 24h Volume: ${pair.volume.h4.toLocaleString('en-US')}</li>
+            <li class="font-metropolis text-white text-4xl border-2 w-80">Holders: PLACEHOLDER</li> 
+            <li class="font-metropolis text-white text-4xl border-2 w-80">Marketcap: ${pair.fdv.toLocaleString('en-US')}</li> 
+            `
+
+            
 
             div.innerHTML = ` this is a test token name is ${pair.baseToken.symbol} <br>
             and the market cap is ${pair.fdv.toLocaleString('en-US')}`
@@ -26,9 +45,10 @@ fetch(TOKEN_URL)
         }
 
         tokenomicsContainter.append(div);
-
+        dataContainer.append(liData);
         // You can add more logic here to display or use the data as needed
     })
     .catch(error => {
         console.error('Error fetching data: ', error);
     });
+*/
